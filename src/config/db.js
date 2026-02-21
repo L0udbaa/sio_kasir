b768388b1b8385c0376cdd1ba63a45612e9fc0f9
L0udbaa <119427139+L0udbaa@users.noreply.github.com>
2026-02-21 23:31:02 +0900

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "api.siodev.sbs",
-  user: process.env.DB_USER || "sio",
-  password: process.env.DB_PASSWORD || "Sio@1234",
-  database: process.env.DB_NAME || "kasir_db",
+  host: process.env.DB_HOST || "",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
